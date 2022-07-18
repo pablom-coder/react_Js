33cabe3817716251facd85e50ac7881839187bab
pablom-coder <npmanrique@gmail.com>
2022-07-18 12:18:17 -0300

    Clase de componentes

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,10 @@
+import './CartWidget.css';
+const CartWidget =()=> {
+    return(
+        <div>
+            <img src='images/carrito.png'  alt="cart"  className="carrito"/>
+              7
+        </div>
+    )
+};
+export default CartWidget;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,18 @@
 import './Navbar.css'
+import CartWidget from '../CartWidget/CartWidget'
 
 const Nav =()=>{
     return(
         <nav className='nav'>
              <ul className='menu'>
-                <li className='logo'><a href="#"></a>REACT</li>
+                <li className='logo'><img src="logo192.png" className="img_logo"></img>{/* <a href="#"></a>REACT */}</li>
                 <li className='item'><a className='a' href="#">Incio</a></li>
                 <li className='item'><a className='a' href="#">Nosotros</a></li>
                 <li className='item'><a className='a' href="#">Servicios</a></li>
                 <li className='item'><a className='a' href="#">Proyectos</a></li>
                 <li className='item'><a className='a' href="#">Contactos</a></li> 
             </ul>
+            <CartWidget/>
         </nav>
     )
 }
