977227c92f1ad2649ae1a2a6788a65f81859b991
pablom-coder <npmanrique@gmail.com>
2022-08-06 18:12:58 -0300

    Clase eventos

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,11 +1,17 @@
 import './ItemDetail.css';
 import ItemCount from '../ItemCount/ItemCount';
-
-const onAdd = (quantity) => {
-    alert("Cantidad de productos agregados " + quantity)
-}
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const ItemDetail = ({product}) => {
+
+    const[quantity, setQuantity] = useState(0)
+
+    const handleOnAdd = (quantity) => {
+        alert("Cantidad de productos agregados " + quantity)
+        setQuantity(quantity)
+    }
+
     return (
         <div key={product.id} className="card_detalle">
             <div className="img_detalle">
@@ -16,7 +22,7 @@ const ItemDetail = ({product}) => {
                 <h3>{product.name}</h3>
                 <h4>$ {product.price}</h4>
                 <div className="texto_detalle_descripcion">
-                    <ItemCount stock={product.stock} initial={0} onAdd={onAdd}/>
+                    {quantity >0 ? <Link to='/cart'>Ir al carrito</Link> : <ItemCount stock={product.stock} initial={0} onAdd={handleOnAdd}/>}
                 </div>
                 
                 <p>{product.description}</p>
